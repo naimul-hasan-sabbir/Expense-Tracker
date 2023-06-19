a7be135ca63d3afd3ff3b5914e6f4a9547381a58
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-06-19 23:40:38 +0600

    spliting

--- lib/main.dart
+++ lib/main.dart
@@ -73,39 +73,6 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2)),
-                    child: Text('\$${tx.amount}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple)),
-                    padding: EdgeInsets.all(10),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18),
-                      ),
-                      Text(
-                        DateFormat.yMMMMd().format(tx.date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  )
-                ]),
-              );
-            }).toList(),
-          ),
         ],
       ),
     );
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -19,6 +19,37 @@ class _TransactionListState extends State<TransactionList> {
   ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: transactions.map((tx) {
+        return Card(
+          child: Row(children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width: 2)),
+              child: Text('\$${tx.amount}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple)),
+              padding: EdgeInsets.all(10),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  tx.title,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                Text(
+                  DateFormat.yMMMMd().format(tx.date),
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            )
+          ]),
+        );
+      }).toList(),
+    );
   }
 }
